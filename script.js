65f17e8fa613031050647b87c1f56f30bed9aca7
Utkir <utkirkurbanov2700@gmail.com>
2022-09-29 17:40:00 +0500

    JavaScript Switch Case operators

---
+++ script.js
@@ -0,0 +1,11 @@
+var carColor = "pink";
+
+if (carColor === "green") {
+  console.log("The cars color is green");
+} else if (carColor === "yellow") {
+  console.log("The car color is yellow");
+} else if (carColor === "red") {
+  console.log("The car color is red");
+} else {
+  console.log("The car color is undifined");
+}
